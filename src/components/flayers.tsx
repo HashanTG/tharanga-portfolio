46db12ec2b46700bcecd2bac167c74ea8df6ceef
Hashan Tharanga Gamage <109969514+HashanTG@users.noreply.github.com>
2024-11-27 13:36:05 +0530

    i add images for the list

--- src/components/flayers.tsx
+++ src/components/flayers.tsx
@@ -2,13 +2,13 @@ import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 
 const imageUrls = [
   "/public/flayers/2024 sinhala and tamil new year.png",
-  "/public/flayers/2024 sinhala and tamil new year.png",
-  "/public/flayers/2024 sinhala and tamil new year.png",
-  "/public/flayers/2024 sinhala and tamil new year.png",
-  "/public/flayers/2024 sinhala and tamil new year.png",
-  "/public/flayers/2024 sinhala and tamil new year.png",
-  "/public/flayers/2024 sinhala and tamil new year.png",
-  "/public/flayers/2024 sinhala and tamil new year.png",
+  "/public/flayers/1717598158540.jpg",
+  "/public/flayers/5456564.png",
+  "/public/flayers/adv.png",
+  "/public/flayers/advsaf.png",
+  "/public/flayers/eid mubarak.png",
+  "/public/flayers/ess_agm_common.png",
+  "/public/flayers/final1223.png",
 
   // Add more image paths
 ];
