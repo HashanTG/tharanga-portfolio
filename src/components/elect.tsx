37c50058c76ec3b69abffeb1fcb8b498d67fa86a
Hashan Tharanga Gamage <109969514+HashanTG@users.noreply.github.com>
2024-11-29 13:47:52 +0530

    i add images for the list

--- src/components/elect.tsx
+++ src/components/elect.tsx
@@ -4,15 +4,33 @@ function Elect() {
   return (
     <div>
       <div className="grid grid-cols-10 grid-rows-8 gap-4">
-        <div className="col-span-4 row-span-6"><img src="/public/electpro/1.png" alt="" /></div>
-        <div className="col-span-2 row-span-3 col-start-5"></div>
-        <div className="col-span-4 row-span-6 col-start-7 row-start-3"></div>
-        <div className="col-span-2 row-span-3 col-start-5 row-start-6"></div>
-        <div className="col-span-2 row-span-2 col-start-5 row-start-4"></div>
-        <div className="col-span-2 row-span-2 col-start-7 row-start-1"></div>
-        <div className="col-span-2 row-span-2 col-start-9 row-start-1"></div>
-        <div className="col-span-2 row-span-2 row-start-7"></div>
-        <div className="col-span-2 row-span-2 col-start-3 row-start-7"></div>
+        <div className="col-span-4 row-span-6">
+          <img src="/public/electpro/1.png" alt="" />
+        </div>
+        <div className="col-span-2 row-span-3 col-start-5">
+          <img src="/public/electpro/2.png" alt="" />
+        </div>
+        <div className="col-span-4 row-span-6 col-start-7 row-start-3">
+          <img src="/public/electpro/3.png" alt="" />
+        </div>
+        <div className="col-span-2 row-span-3 col-start-5 row-start-6">
+          <img src="/public/electpro/4.png" alt="" />
+        </div>
+        <div className="col-span-2 row-span-2 col-start-5 row-start-4">
+          <img src="/public/electpro/5.png" alt="" />
+        </div>
+        <div className="col-span-2 row-span-2 col-start-7 row-start-1">
+          <img src="/public/electpro/6.png" alt="" />
+        </div>
+        <div className="col-span-2 row-span-2 col-start-9 row-start-1">
+          <img src="/public/electpro/7.png" alt="" />
+        </div>
+        <div className="col-span-2 row-span-2 row-start-7">
+          <img src="/public/electpro/8.png" alt="" />
+        </div>
+        <div className="col-span-2 row-span-2 col-start-3 row-start-7">
+          <img src="/public/electpro/9.png" alt="" />
+        </div>
       </div>
     </div>
   );
